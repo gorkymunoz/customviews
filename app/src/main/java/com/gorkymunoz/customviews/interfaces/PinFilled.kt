a4711e3a594c6865a327100203bd77e6f960701c
Gorky Muñoz <gorkymunoz@hotmail.com>
2021-06-11 14:35:33 -0500

    Listener to notify when pin is completed

---
+++ app/src/main/java/com/gorkymunoz/customviews/interfaces/PinFilled.kt
@@ -0,0 +1,12 @@
+package com.gorkymunoz.customviews.interfaces
+
+
+/**
+ * Created by Gorky Muñoz on 11/6/2021.
+ * Notifies when the pin is completed
+ * gorkymunoz@hotmail.com
+ */
+interface PinFilled {
+
+    fun pinCompleted(pin: String)
+}
