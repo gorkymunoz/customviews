c125b5f4634c3a2f6cbca72f80d1b4d1d0e9c4e4
Gorky Muñoz <gorkymunoz@hotmail.com>
2021-06-11 10:10:16 -0500

    Main activity with layout

--- app/src/main/java/com/gorkymunoz/customviews/MainActivity.kt
+++ app/src/main/java/com/gorkymunoz/customviews/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.gorkymunoz.customviews
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gorkymunoz.customviews.enum.DotState
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    var state: DotState = DotState.EMPTY
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv_hello.setOnClickListener {
+            state = state.next()
+            dotview.setDotState(state)
+        }
     }
+
+
 }
