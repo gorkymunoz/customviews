7620a53ded841369ee7651312f6c5e225c4b717a
Gorky Muñoz <gorkymunoz@hotmail.com>
2021-07-06 18:15:48 -0500

    Generic method to get the digital signature of application

--- app/src/main/java/com/gorkymunoz/customviews/MainActivity.kt
+++ app/src/main/java/com/gorkymunoz/customviews/MainActivity.kt
@@ -3,22 +3,19 @@ package com.gorkymunoz.customviews
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.gorkymunoz.customviews.enum.DotState
 import com.gorkymunoz.customviews.interfaces.PinFilled
+import com.gorkymunoz.customviews.utils.SignatureUtils
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), PinFilled {
-
-
-    var state: DotState = DotState.EMPTY
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        SignatureUtils.getKeyHash(this, "SHA-256")
+
         tv_add.setOnClickListener {
-            state = state.next()
-            dotview.setDotState(state)
             dotlayout.addPinEntry("1")
         }
 
---
+++ app/src/main/java/com/gorkymunoz/customviews/utils/SignatureUtils.java
@@ -0,0 +1,73 @@
+package com.gorkymunoz.customviews.utils;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Build;
+import android.util.Base64;
+import android.util.Log;
+
+import com.gorkymunoz.customviews.BuildConfig;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by Gorky Muñoz on 6/7/2021.
+ * <p>
+ * gorkymunoz@hotmail.com
+ */
+public class SignatureUtils {
+
+    public static final String TAG = SignatureUtils.class.getSimpleName();
+
+    @SuppressLint("PackageManagerGetSignatures")
+    public static void getKeyHash(Context context, String hashStrategy) {
+        PackageInfo info;
+        Signature[] signatures;
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                info = context.getPackageManager().getPackageInfo(BuildConfig.APPLICATION_ID, PackageManager.GET_SIGNING_CERTIFICATES);
+                signatures = info.signingInfo.getApkContentsSigners();
+            } else {
+                info = context.getPackageManager().getPackageInfo(BuildConfig.APPLICATION_ID, PackageManager.GET_SIGNATURES);
+                signatures = info.signatures;
+            }
+            for (Signature signature : signatures) {
+                final MessageDigest md = MessageDigest.getInstance(hashStrategy);
+                md.update(signature.toByteArray());
+
+                final byte[] digest = md.digest();
+                final StringBuilder toRet = new StringBuilder();
+                for (int i = 0; i < digest.length; i++) {
+                    if (i != 0) toRet.append(":");
+                    int b = digest[i] & 0xff;
+                    String hex = Integer.toHexString(b);
+                    if (hex.length() == 1) toRet.append("0");
+                    toRet.append(hex);
+                }
+
+                StringBuilder sb = new StringBuilder();
+                for (byte b : digest) {
+                    sb.append(Integer.toHexString(0xff & b));
+                }
+
+                Log.e("Key1", hashStrategy + "-->>>>>>>>>>>>" + new String(Base64.encode(digest, 0)));
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    Log.e("Key2", hashStrategy + "-->>>>>>>>>>>>" + java.util.Base64.getEncoder().encodeToString(digest));
+                }
+                Log.e("KeyHash", hashStrategy + "-->>>>>>>>>>>>" + toRet.toString());
+                Log.e("KeyHex", hashStrategy + "-->>>>>>>>>>>>" + sb.toString());
+                // Notification.registerGCM(this);
+            }
+        } catch (PackageManager.NameNotFoundException e1) {
+            Log.e(TAG, "name not found::", e1);
+        } catch (NoSuchAlgorithmException e) {
+            Log.e(TAG, "no such an algorithm::" + e);
+        } catch (Exception e) {
+            Log.e(TAG, "exception", e);
+        }
+    }
+}
