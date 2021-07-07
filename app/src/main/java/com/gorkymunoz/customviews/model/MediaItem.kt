0f8c50bdebbec839142d8b2802e093bfb36c11bb
Gorky Muñoz <gorkymunoz@hotmail.com>
2021-07-06 20:10:57 -0500

    MediaItem data class & data provider

---
+++ app/src/main/java/com/gorkymunoz/customviews/model/MediaItem.kt
@@ -0,0 +1,9 @@
+package com.gorkymunoz.customviews.model
+
+
+/**
+ * Created by Gorky Muñoz on 6/7/2021.
+ *
+ * gorkymunoz@hotmail.com
+ */
+data class MediaItem(val title: String, val url: String)
