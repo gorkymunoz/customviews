7c31a82266ce70eb782bd58d134a08ec6911bba4
Gorky Muñoz <gorkymunoz@hotmail.com>
2021-07-06 20:11:47 -0500

    add adapter to recyclerview

--- app/src/main/java/com/gorkymunoz/customviews/MainActivity.kt
+++ app/src/main/java/com/gorkymunoz/customviews/MainActivity.kt
@@ -3,11 +3,15 @@ package com.gorkymunoz.customviews
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.gorkymunoz.customviews.adapters.MediaAdapter
+import com.gorkymunoz.customviews.data.getItems
+import com.gorkymunoz.customviews.interfaces.Logger
 import com.gorkymunoz.customviews.interfaces.PinFilled
 import com.gorkymunoz.customviews.utils.SignatureUtils
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), PinFilled {
+class MainActivity : AppCompatActivity(), PinFilled, Logger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,12 +27,22 @@ class MainActivity : AppCompatActivity(), PinFilled {
             dotlayout.deletePinEntry()
         }
 
+        val recycler: RecyclerView = findViewById(R.id.rv)
+        logD("Initializing recycler")
+        recycler.apply {
+            adapter = MediaAdapter(getItems())
+        }
+
         dotlayout.setListener(this)
     }
 
     override fun pinCompleted(pin: String) {
         Toast.makeText(this, "Pin is $pin", Toast.LENGTH_LONG).show()
         dotlayout.showError()
+    }
+
+    fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
 
