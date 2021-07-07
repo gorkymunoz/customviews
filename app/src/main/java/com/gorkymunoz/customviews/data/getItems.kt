2a9e644921487ff35421d11a0ed08621be4b30a7
Gorky Muñoz <gorkymunoz@hotmail.com>
2021-07-06 20:10:57 -0500

    MediaItem data class & data provider

---
+++ app/src/main/java/com/gorkymunoz/customviews/data/getItems.kt
@@ -0,0 +1,24 @@
+package com.gorkymunoz.customviews.data
+
+import com.gorkymunoz.customviews.model.MediaItem
+
+
+/**
+ * Created by Gorky Muñoz on 6/7/2021.
+ *
+ * gorkymunoz@hotmail.com
+ */
+fun getItems(): List<MediaItem> = listOf(
+    MediaItem("Title 1", "https://placekitten.com/200/300?image=1"),
+    MediaItem("Title 2", "https://placekitten.com/200/300?image=2"),
+    MediaItem("Title 3", "https://placekitten.com/200/300?image=3"),
+    MediaItem("Title 4", "https://placekitten.com/200/300?image=4"),
+    MediaItem("Title 5", "https://placekitten.com/200/300?image=5"),
+    MediaItem("Title 6", "https://placekitten.com/200/300?image=6"),
+    MediaItem("Title 7", "https://placekitten.com/200/300?image=7"),
+    MediaItem("Title 8", "https://placekitten.com/200/300?image=8"),
+    MediaItem("Title 9", "https://placekitten.com/200/300?image=9"),
+    MediaItem("Title 10", "https://placekitten.com/200/300?image=10"),
+
+
+    )
