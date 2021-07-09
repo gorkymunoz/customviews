ec236e3fcbd5e2d92b3dee139816912918e748c7
Gorky Muñoz <gorkymunoz@hotmail.com>
2021-07-09 15:50:42 -0500

    Using apply to initialize binding

--- app/src/main/java/com/gorkymunoz/customviews/MainActivity.kt
+++ app/src/main/java/com/gorkymunoz/customviews/MainActivity.kt
@@ -2,43 +2,45 @@ package com.gorkymunoz.customviews
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
 import com.gorkymunoz.customviews.adapters.MediaAdapter
 import com.gorkymunoz.customviews.data.getItems
+import com.gorkymunoz.customviews.databinding.ActivityMainBinding
 import com.gorkymunoz.customviews.extensions.toast
 import com.gorkymunoz.customviews.interfaces.Logger
 import com.gorkymunoz.customviews.interfaces.PinFilled
 import com.gorkymunoz.customviews.utils.SignatureUtils
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), PinFilled, Logger {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            tvAdd.setOnClickListener {
+                dotlayout.addPinEntry("1")
+            }
+
+            tvRemove.setOnClickListener {
+                dotlayout.deletePinEntry()
+            }
+
+            rv.apply {
+                adapter = MediaAdapter(getItems())
+            }
+
+            dotlayout.setListener(this@MainActivity)
+
+            setContentView(root)
+        }
+
 
         SignatureUtils.getKeyHash(this, "SHA-256")
-
-        tv_add.setOnClickListener {
-            dotlayout.addPinEntry("1")
-        }
-
-        tv_remove.setOnClickListener {
-            dotlayout.deletePinEntry()
-        }
-
-        val recycler: RecyclerView = findViewById(R.id.rv)
-        logD("Initializing recycler")
-        recycler.apply {
-            adapter = MediaAdapter(getItems())
-        }
-
-        dotlayout.setListener(this)
     }
 
     override fun pinCompleted(pin: String) {
         toast("Pin is $pin")
-        dotlayout.showError()
+        binding.dotlayout.showError()
     }
 
 
