20664c9a824cf7b8083ba6d07094c57504f69174
Gorky Muñoz <gorkymunoz@hotmail.com>
2021-07-09 15:50:00 -0500

    Added type to media item

--- app/src/main/java/com/gorkymunoz/customviews/model/MediaItem.kt
+++ app/src/main/java/com/gorkymunoz/customviews/model/MediaItem.kt
@@ -1,9 +1,11 @@
 package com.gorkymunoz.customviews.model
 
+import com.gorkymunoz.customviews.enum.MediaType
+
 
 /**
  * Created by Gorky Muñoz on 6/7/2021.
  *
  * gorkymunoz@hotmail.com
  */
-data class MediaItem(val title: String, val url: String)
+data class MediaItem(val title: String, val url: String, val type: MediaType)
