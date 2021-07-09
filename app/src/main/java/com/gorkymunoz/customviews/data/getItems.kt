c661e28ae167fb36d18b0b6fe011552e4666fa05
Gorky Muñoz <gorkymunoz@hotmail.com>
2021-07-09 15:50:00 -0500

    Added type to media item

--- app/src/main/java/com/gorkymunoz/customviews/data/getItems.kt
+++ app/src/main/java/com/gorkymunoz/customviews/data/getItems.kt
@@ -1,5 +1,6 @@
 package com.gorkymunoz.customviews.data
 
+import com.gorkymunoz.customviews.enum.MediaType
 import com.gorkymunoz.customviews.model.MediaItem
 
 
@@ -9,16 +10,16 @@ import com.gorkymunoz.customviews.model.MediaItem
  * gorkymunoz@hotmail.com
  */
 fun getItems(): List<MediaItem> = listOf(
-    MediaItem("Title 1", "https://placekitten.com/200/300?image=1"),
-    MediaItem("Title 2", "https://placekitten.com/200/300?image=2"),
-    MediaItem("Title 3", "https://placekitten.com/200/300?image=3"),
-    MediaItem("Title 4", "https://placekitten.com/200/300?image=4"),
-    MediaItem("Title 5", "https://placekitten.com/200/300?image=5"),
-    MediaItem("Title 6", "https://placekitten.com/200/300?image=6"),
-    MediaItem("Title 7", "https://placekitten.com/200/300?image=7"),
-    MediaItem("Title 8", "https://placekitten.com/200/300?image=8"),
-    MediaItem("Title 9", "https://placekitten.com/200/300?image=9"),
-    MediaItem("Title 10", "https://placekitten.com/200/300?image=10"),
+    MediaItem("Title 1", "https://placekitten.com/200/300?image=1", MediaType.PHOTO),
+    MediaItem("Title 2", "https://placekitten.com/200/300?image=2", MediaType.VIDEO),
+    MediaItem("Title 3", "https://placekitten.com/200/300?image=3", MediaType.PHOTO),
+    MediaItem("Title 4", "https://placekitten.com/200/300?image=4", MediaType.PHOTO),
+    MediaItem("Title 5", "https://placekitten.com/200/300?image=5", MediaType.VIDEO),
+    MediaItem("Title 6", "https://placekitten.com/200/300?image=6", MediaType.PHOTO),
+    MediaItem("Title 7", "https://placekitten.com/200/300?image=7", MediaType.PHOTO),
+    MediaItem("Title 8", "https://placekitten.com/200/300?image=8", MediaType.VIDEO),
+    MediaItem("Title 9", "https://placekitten.com/200/300?image=9", MediaType.PHOTO),
+    MediaItem("Title 10", "https://placekitten.com/200/300?image=10", MediaType.PHOTO),
 
 
     )
---
+++ app/src/main/java/com/gorkymunoz/customviews/enum/MediaType.kt
@@ -0,0 +1,11 @@
+package com.gorkymunoz.customviews.enum
+
+
+/**
+ * Created by Gorky Muñoz on 6/7/2021.
+ *
+ * gorkymunoz@hotmail.com
+ */
+enum class MediaType {
+    PHOTO, VIDEO
+}
