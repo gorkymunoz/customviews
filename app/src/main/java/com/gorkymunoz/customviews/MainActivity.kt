13930cc3940976f331ee1be1e8f534114d4210cf
Gorky Muñoz <gorkymunoz@hotmail.com>
2021-07-16 16:34:44 -0500

    create filter for media type items

--- app/src/main/java/com/gorkymunoz/customviews/MainActivity.kt
+++ app/src/main/java/com/gorkymunoz/customviews/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.gorkymunoz.customviews
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.gorkymunoz.customviews.adapters.MediaAdapter
 import com.gorkymunoz.customviews.data.getItems
 import com.gorkymunoz.customviews.databinding.ActivityMainBinding
+import com.gorkymunoz.customviews.enum.MediaType
 import com.gorkymunoz.customviews.extensions.toast
 import com.gorkymunoz.customviews.interfaces.Logger
 import com.gorkymunoz.customviews.utils.SignatureUtils
@@ -13,7 +16,7 @@ class MainActivity : AppCompatActivity(), Logger {
 
     private lateinit var binding: ActivityMainBinding
     private val mediaAdapter: MediaAdapter by lazy {
-        MediaAdapter(getItems()) {
+        MediaAdapter {
             toast(it.title)
         }
     }
@@ -33,6 +36,8 @@ class MainActivity : AppCompatActivity(), Logger {
                 adapter = mediaAdapter
             }
 
+            mediaAdapter.items = getItems()
+
             dotlayout.setListener {
                 toast("Pin is $it")
                 binding.dotlayout.showError()
@@ -43,6 +48,23 @@ class MainActivity : AppCompatActivity(), Logger {
 
 
         SignatureUtils.getKeyHash(this, "SHA-256")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        mediaAdapter.items = getItems().let { items ->
+            when (item.itemId) {
+                R.id.filter_videos -> items.filter { it.type == MediaType.VIDEO }
+                R.id.filter_photos -> items.filter { it.type == MediaType.PHOTO }
+                R.id.filter_all -> items
+                else -> emptyList()
+            }
+        }
+        return true
     }
 
 }
